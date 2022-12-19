24a0c5d624fd1f38c2eda2bed8e867e4c39007e7
Jungie29 <112969101+Jungie29@users.noreply.github.com>
2022-12-19 17:21:33 +0800

    Add files via upload

---
+++ Portfolio/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <script src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <title>JFrancisco</title>
+</head>
+
+<body style="background-color: grey;">
+  <div id="box" class="column col-md-6" style="padding-bottom: 75px;">
+    <center>
+      <div class="column col-md-6" style="float: left;">
+        <img src="img/Junz.jpg" alt="" class="box-img">
+      </div>
+      <br>
+      <h1 style="font-size: 25px;">Jungie R. Francisco</h1>
+      <h5>Photographer - Graphic Designer</h5>
+      <p>An aspiring graphic designer and front-end developer</p>
+      <ul>
+        <a href="https://www.facebook.com/akosijungie"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+        <a href="https://www.instagram.com/thekingofthejungie "><i class="fa fa-instagram ft14"
+            aria-hidden="true"></i></a>
+      </ul>
+      <div class="row">
+        <h2>Educational Background</h5>
+          <div class="col-sm-6">
+            <img src="img/clsu.png" alt="" style="width: 30%;">
+            <h5 class="card-title">Central Luzon State University</h5>
+          </div>
+          <div class="col-sm-6">
+            <img src="img/tnhs.png" alt="" style="width: 30%;">
+            <h5 class="card-title">Talavera National High School</h5>
+          </div>
+      </div>
+    </center>
+  </div>
+  <br>
+  <center>
+    <h3 style="font-family: Poppins;"><b>Projects</b></h3>
+  </center>
+  <br><br><br>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">Tote Bag Mockup</div>
+          <div class="card-body">
+            <img src="img/img3.png" alt="pubmat">
+            <p class="card-text">Tote bag </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">BITS Poster</div>
+          <div class="card-body">
+            <img src="img/bitspubmat.png" alt="pubmat">
+            <p class="card-text">Poster made for the upcoming event for BITS</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">BITS Poster</div>
+          <div class="card-body">
+            <!-- Trigger the Modal -->
+            <img id="myImg" src="img/bitsposter.png" alt="poster" style="max-width:18rem;">
+            
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+            
+              <!-- The Close Button -->
+              <span class="close">&times;</span>
+            
+              <!-- Modal Content (The Image) -->
+              <img class="modal-content" id="img01">
+            
+              <!-- Modal Caption (Image Text) -->
+              <div id="caption"></div>
+            </div>
+            <p class="card-text">Poster made for the upcoming event for BITS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">Logo</div>
+          <div class="card-body">
+            <img src="img/img1.png" alt="pubmat">
+            <p class="card-text">Poster made for the upcoming event for BITS</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">SKO Certificate</div>
+          <div class="card-body">
+            <img src="img/SKO/participation.png" alt="pubmat">
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">Prototype for capstone project</div>
+          <div class="card-body">
+            <img src="img/Prototype.png" alt="pubmat">
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">Figma Prototype</div>
+          <div class="card-body">
+            <img src="img/Prototype.png" alt="pubmat">
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">SKO Certificate</div>
+          <div class="card-body">
+            <p class="card-text">Poster made for the upcoming event for BITS</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">BITS Poster</div>
+          <div class="card-body">
+            <p class="card-text">Poster made for the upcoming event for BITS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  php
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js"> </script>
+</body>
+
+</html>
